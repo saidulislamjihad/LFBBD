47761c89c2a7ac5b20228fdb512e4b7de31c41f0
Saidul Islam <saidulislamjihad20@gmail.com>
2023-05-07 19:33:40 +0600

    fix:Some section design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,53 @@
-//Product Slider
+//About Slider
 $('.about-award-wrap').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+//Customer Slider
+$('.customer-items-wrap').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+//Testimonial Slider
+$('.testimonial-items-wrap').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
