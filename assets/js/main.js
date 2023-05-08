da07a6773cba50f0f4f12ce1b39e6b3d7727bc96
Saidul Islam <saidulislamjihad20@gmail.com>
2023-05-08 13:02:37 +0600

    Some section design

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,27 @@ $('.testimonial-items-wrap').owlCarousel({
         }
     }
 });
+
+//Case Study Slider
+$('.case-study-items-wrap').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+});
